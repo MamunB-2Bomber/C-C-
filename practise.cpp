c2d165bd49b5917eb6307823c53ea5516514b6fa
Humble Fool <70875370+MamunB-2Bomber@users.noreply.github.com>
2020-09-07 12:53:18 +0600

    Add files via upload

---
+++ practise.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i,j,k,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				for(k=j;k<n;k++)
+				{
+					a[k]=a[k+1];
+				}
+					n--;
+					j--;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+}
